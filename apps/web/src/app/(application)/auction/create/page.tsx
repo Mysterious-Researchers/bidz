1c93456dedc31d67dafaee3a023460375bb61e3a
kujo205 <kuc8301@gmail.com>
2024-02-08 18:22:53 +0200

    chore: added react-hook-form && changed .gitignore

--- apps/web/src/app/(application)/auction/create/page.tsx
+++ apps/web/src/app/(application)/auction/create/page.tsx
@@ -1,7 +1,8 @@
-export default function HomePage() {
-  return (
-    <main className="flex min-h-[calc(100vh-480px)] flex-1 flex-col bg-slate-50">
-      creating an auction
-    </main>
-  );
+export default function Page() {
+  return (<>
+
+
+
+
+  </>);
 }
