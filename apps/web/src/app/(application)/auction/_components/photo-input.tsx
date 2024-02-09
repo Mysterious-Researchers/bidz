1ff05351680061fae35118eae48906720cd07632
kujo205 <kuc8301@gmail.com>
2024-02-09 15:52:51 +0200

    style(front-end): chanhed add photo input bg color

--- apps/web/src/app/(application)/auction/_components/photo-input.tsx
+++ apps/web/src/app/(application)/auction/_components/photo-input.tsx
@@ -43,7 +43,7 @@ const PhotoInput = ({ key, onPhotoAdd }: PhotoInputProps) => {
     <div className="flex h-[130px] w-[200px] items-center justify-center">
       <label
         htmlFor="dropzone-file"
-        className="dark:hover:bg-bray-800 flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        className="dark:hover:bg-bray-800 flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-blue-50 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
       >
         <div className="flex flex-col items-center justify-center px-5">
           <Icons.Upload />
