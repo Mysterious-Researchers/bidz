805d6711c32eaed65ac2ec5f191a2f4434f6e905
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-02-10 19:37:36 +0200

    feature: created endpoint for getting bids

--- apps/api/src/api/services/auction.service.ts
+++ apps/api/src/api/services/auction.service.ts
@@ -1,16 +1,30 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Message } from '../../database/entities/message.entity';
 import {User} from "../../database/entities/user.entity";
+import {Bid} from "../../database/entities/bid.entity";
 
 @Injectable()
 export class AuctionService {
   constructor(
     @Inject('MESSAGES_REPOSITORY')
     private messageModel: typeof Message,
+
+    @Inject('BIDS_REPOSITORY')
+    private bidModel: typeof Bid,
   ) {}
 
   async getAuctionMessages(auctionId: string) {
     return this.messageModel.findAll({
+      where: {
+        auctionId,
+      },
+      order: [['createdAt', 'ASC']],
+      include: [{ model: User }],
+    });
+  }
+
+  async getBids(auctionId: string) {
+    return this.bidModel.findAll({
       where: {
         auctionId,
       },
