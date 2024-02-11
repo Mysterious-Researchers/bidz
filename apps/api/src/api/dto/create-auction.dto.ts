ad52c2a4ad2fefb5b0645945e536807ac24cc91c
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-02-11 16:24:39 +0200

    fix: fixed endpoint for creating auctions

--- apps/api/src/api/dto/create-auction.dto.ts
+++ apps/api/src/api/dto/create-auction.dto.ts
@@ -1,12 +1,9 @@
-import { Photo } from '../../database/entities/photo.entity';
-
 export class CreateAuctionDto {
   name: string;
   userId: string;
   startPrice: number;
   stepPrice: number;
-  currentPrice: number;
   endDate: string;
   description: string;
-  photos: Photo[];
+  photos: string[];
 }
